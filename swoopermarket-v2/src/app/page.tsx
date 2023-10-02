1e677501e5aef7f3009b9840ba44054a77eec16b
Mei Deng <meideng06@gmail.com>
2023-10-02 10:22:33 -0400

    Listings +SingleItem components

--- swoopermarket-v2/src/app/page.tsx
+++ swoopermarket-v2/src/app/page.tsx
@@ -5,16 +5,16 @@ import Drawer from '@mui/material/Drawer';
 import Typography from '@mui/material/Typography';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
-import MediaCard from '@/components/MediaCard';
 import Link from 'next/link';
-import SearchBar from '@/components/SearchBar';
+import SearchBar from '@/components/HomePage/SearchBar';
+import Listings from '@/components/HomePage/Listings';
 
 export default function HomePage() {
   return (
     <Box sx={{ display: 'flex', alignItems: 'flex-start'}}>
       <div>
         <Grid id="header" container direction="row" justifyContent="space-between" alignItems="center" padding="24px 0px">
-          <Typography variant="h5" sx={{color: "#0033a0", paddingTop: 1}}>
+          <Typography variant="h5" sx={{color: "#0033a0"}}>
             <Link 
                 href="/" 
                 style={{
@@ -27,32 +27,7 @@ export default function HomePage() {
           </Typography>
           <SearchBar placeHolderText="Search SwooperMarket"/>
         </Grid>
-        <Grid id="listings" container rowSpacing={3} columnSpacing={3}>
-          <Grid xs={6}>
-            <MediaCard
-              heading="CMYK"
-              text="The CMYK color model (also known as process color, or four color) is a subtractive color model, based on the CMY color model, used in color printing, and is also used to describe the printing process itself."
-            />
-          </Grid>
-          <Grid xs={6}>
-            <MediaCard
-              heading="HSL and HSV"
-              text="HSL (for hue, saturation, lightness) and HSV (for hue, saturation, value; also known as HSB, for hue, saturation, brightness) are alternative representations of the RGB color model, designed in the 1970s by computer graphics researchers."
-            />
-          </Grid>
-          <Grid xs={6}>
-            <MediaCard
-              heading="RGB"
-              text="An RGB color space is any additive color space based on the RGB color model. RGB color spaces are commonly found describing the input signal to display devices such as television screens and computer monitors."
-            />
-          </Grid>
-          <Grid xs={6}>
-            <MediaCard
-              heading="CIELAB"
-              text="The CIELAB color space, also referred to as L*a*b*, was intended as a perceptually uniform space, where a given numerical change corresponds to a similar perceived change in color."
-            />
-          </Grid>
-        </Grid>
+        <Listings/>
       </div>
     </Box>
   );
