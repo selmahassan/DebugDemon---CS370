41e6db7235402a64e74cbc7a5c3a9b66d0427bd5
Kathy Ning <kathyning@Kathys-MacBook-Air.local>
2023-10-15 21:08:02 -0400

    clean up unused imports

--- swoopermarket-v2/src/app/layout.tsx
+++ swoopermarket-v2/src/app/layout.tsx
@@ -1,21 +1,5 @@
 import * as React from 'react';
-import Link from 'next/link';
-import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import HomeIcon from '@mui/icons-material/Home';
-import AddIcon from '@mui/icons-material/Add';
-import ChatIcon from '@mui/icons-material/Chat';
-import ManageAccountsIcon from '@mui/icons-material/ManageAccounts';
-import LogoutIcon from '@mui/icons-material/Logout';
 import ThemeRegistry from '@/components/ThemeRegistry/ThemeRegistry';
 import NavBar from '@/components/NavBar/NavBar';
 
@@ -25,17 +9,6 @@ export const metadata = {
 };
 
 const DRAWER_WIDTH = 240;
-
-const LINKS = [
-  { text: 'Home', href: '/', icon: HomeIcon },
-  { text: 'New Listing', href: '/newlisting', icon: AddIcon },
-  { text: 'Direct Messages', href: '/dms', icon: ChatIcon },
-];
-
-const PLACEHOLDER_LINKS = [
-  { text: 'Profile', href: '/profile', icon: ManageAccountsIcon },
-  { text: 'Logout', href: '/login', icon: LogoutIcon },
-];
 
 /* Creates project skeleton with app header and navigation bar */
 export default function RootLayout({ children }: { children: React.ReactNode }) {
