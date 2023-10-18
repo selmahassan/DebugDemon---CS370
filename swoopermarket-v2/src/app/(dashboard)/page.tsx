f6ef75226ef4137c59f6cdb59fce687c530fa896
Kathy Ning <kathyning@Kathys-MacBook-Air-2.local>
2023-10-18 13:27:22 -0400

    remove unused imports

--- swoopermarket-v2/src/app/(dashboard)/page.tsx
+++ swoopermarket-v2/src/app/(dashboard)/page.tsx
@@ -1,11 +1,7 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Unstable_Grid2';
-import Drawer from '@mui/material/Drawer';
 import Typography from '@mui/material/Typography';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import Link from 'next/link';
 import SearchBar from '@/components/HomePage/SearchBar';
 import Listings from '@/components/HomePage/Listings';
 
