cea5fac734f372193bffdf010b8de00505209c46
Kathy Ning <kathyning@Kathys-MacBook-Air-2.local>
2023-10-18 14:09:11 -0400

    add forgot-password page)

--- swoopermarket-v2/src/app/(login)/forgot-password/page.tsx
+++ swoopermarket-v2/src/app/(login)/forgot-password/page.tsx
@@ -4,8 +4,6 @@ import * as React from 'react';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
 import Link from 'next/link';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
@@ -14,7 +12,7 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import { useRouter } from 'next/navigation'
 
-export default function Login() {
+export default function ForgotPassword() {
   const router = useRouter()
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
@@ -22,9 +20,8 @@ export default function Login() {
     const data = new FormData(event.currentTarget);
     console.log({
       email: data.get('email'),
-      password: data.get('password'),
     });
-    router.push('/') //TODO: Redirect
+    router.push('/login') //TODO: Redirect
   };
 
   return (
@@ -46,7 +43,7 @@ export default function Login() {
       <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
         <Box
           sx={{
-            my: 15,
+            my: 25,
             mx: 4,
             display: 'flex',
             flexDirection: 'column',
@@ -56,36 +53,21 @@ export default function Login() {
           <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
             <LockOutlinedIcon />
           </Avatar>
-          <Typography variant="h5" sx={{color: "#0033a0", paddingTop: 1}}>
-            Welcome to SwooperMarket
-          </Typography>
           <Typography component="h1" variant="h5">
-            Sign in
+            Forgot your Password?
+          </Typography>
+          <Typography component="h5" variant="body2" align='center'>
+            If you have forgotten your password, please enter the account's email address below and click the "Reset My Password" button. You will receive an email that contains a link to set a new password. 
           </Typography>
           <Box component="form" onSubmit={handleSubmit} sx={{ mt: 1 }}>
             <TextField
-              margin="normal"
               required
               fullWidth
+              margin="normal"
               id="email"
               label="Email Address"
               name="email"
-              // autoComplete="email"
               autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              // autoComplete="current-password"
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
             />
             <Button
               type="submit"
@@ -93,17 +75,14 @@ export default function Login() {
               variant="contained"
               sx={{ mt: 3, mb: 2 }}
             >
-              Sign In
+              Reset My Password
             </Button>
             <Grid container>
               <Grid item xs>
-                <Link href="/forgot-password" style={{ textDecoration:"none" }}>
-                  Forgot password?
-                </Link>
               </Grid>
               <Grid item>
-                <Link href="#" style={{ textDecoration:"none" }}>
-                  {"Don't have an account? Sign Up"}
+                <Link href="/login" style={{ textDecoration:"none" }}>
+                  {"Return to Login"}
                 </Link>
               </Grid>
             </Grid>
