d52fd1c0648cb629b055242eed3955d71de09191
selmahassan <78364187+selmahassan@users.noreply.github.com>
2023-10-22 17:51:57 -0400

    Add files via upload

--- swoopermarket-v2/src/api/messaging/getMessages.ts
+++ swoopermarket-v2/src/api/messaging/getMessages.ts
@@ -1,29 +1,27 @@
-// import { getMessages, sendMessage } from "@/types/messageType";
-// import { NextResponse } from "next/server";
+import { sql } from '@vercel/postgres';
+import { getMessages, sendMessage } from "@/types/messageType";
+import { NextResponse } from "next/server";
 
-// // Endpoint to get messages
-// export const GET = async (req: Request, res: Response) => {
-//     try {
-//         const messages = getMessages(); // Replace with your function to retrieve messages
-//         return NextResponse.json({ messages }, { status: 200 });
-//     } catch (error) {
-//         return NextResponse.json({ message: "Error", error }, { status: 500 });
-//     }
-// };
+// Endpoint to get messages
+export const GET = async (req: Request, res: Response) => {
+    try {
+        const messages =  await sql`SELECT * FROM messages;`;
+        return NextResponse.json({ messages }, { status: 200 });
+    } catch (error) {
+        return NextResponse.json({ message: "Error", error }, { status: 500 });
+    }
+};
 
-// // Endpoint to send a message
-// export const POST = async (req: Request, res: Response) => {
-//     try {
-//         const { sender, receiver, text } = await req.json();
-//         const message = {
-//             sender,
-//             receiver,
-//             text,
-//             timestamp: new Date(),
-//         };
-//         sendMessage(message);
-//         return NextResponse.json({ message: "Message sent"}, { status: 201 });
-//     } catch (error) {
-//         return NextResponse.json({ message: "Error", error }, { status: 500 });
-//     }
-// };
+
+// Endpoint to send a message
+export const POST = async (req: Request, res: Response) => {
+    try {
+        const { message_id, userid, listing_id, text_msg } = await req.json();
+        const message = await sql`INSERT INTO messages ((message_id, userid, listing_id, text_msg) VALUES ($1, $2, $3, $4)', [message_id, userid, listing_id, text_msg];`;
+        return NextResponse.json({ message: "Message sent"}, { status: 201 });
+    } catch (error) {
+        return NextResponse.json({ message: "Error", error }, { status: 500 });
+    }
+};
+
+
