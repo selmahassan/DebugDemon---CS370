851defc0b3600edf587a0c18500dff078317c944
Ryan <67213962+ryan-zhao313@users.noreply.github.com>
2023-10-23 14:09:40 -0400

    test bug fixes

--- swoopermarket-v2/src/app/(dashboard)/profile/page.tsx
+++ swoopermarket-v2/src/app/(dashboard)/profile/page.tsx
@@ -7,6 +7,7 @@ import Button from '@mui/material/Button';
 import Avatar from '@mui/material/Avatar';
 import Rating from '@mui/material/Rating';
 import ProfileListings from "@/components/ProfilePage/ProfileListings";
+import Listings from '@/components/HomePage/Listings';
 
 export default function ProfilePage() {
   return (
@@ -15,17 +16,18 @@ export default function ProfilePage() {
       <Stack direction="row" sx={{pl: 2, pr: 2}} justifyContent="flex-start" alignItems="center" spacing={2}>
         <Button href="/" sx={{borderRadius: 50, width: "fit-content"}} startIcon={<ArrowBack/>}>Back to listings</Button>
       </Stack>
-      <Stack direction="column" spacing={1} alignItems="center" >
-        <Avatar alt="Profile Picture" src={"/images/avatar/gru.jpg"} sx={{ width: 200, height: 200 }} />
-        <Rating name="read-only" defaultValue={1.5} precision={0.5} readOnly />
-        <Typography variant="h4" color="initial">
-          Gru
-        </Typography>
-        <Typography variant="body1" color="initial">
-          Email: iWantTostealTheMoon@minions.com
-        </Typography>
-      </Stack>
-      <ProfileListings/>
+        <Stack direction="column" spacing={1} alignItems="center" >
+            <Avatar alt="Profile Picture" src={"/images/avatar/gru.jpg"} sx={{ width: 200, height: 200 }} />
+            <Rating name="read-only" defaultValue={1.5} precision={0.5} readOnly />
+            <Typography variant="h4" color="initial">
+              Gru
+            </Typography>
+            <Typography variant="body1" color="initial">
+              Email: iWantTostealTheMoon@minions.com
+            </Typography>
+        </Stack>
+
+        <ProfileListings/>
     </div>
   );
 }
--- swoopermarket-v2/src/components/ProfilePage/ProfileListings.tsx
+++ swoopermarket-v2/src/components/ProfilePage/ProfileListings.tsx
@@ -1,17 +1,13 @@
-import Grid from '@mui/material/Unstable_Grid2';
+import { Typography, Grid } from '@mui/material';
 import ListingCard from '@/components/HomePage/ListingCard';
-import { Typography } from '@mui/material';
 
-// TODO: Connect to the database to get real listings from the user
-
-const singleItems = [
+const profileItems = [
     {
         id: 1,
         description: "Pet Alligator",
         price: 48,
         condition: "Like New",
         src: "/images/profileListings/alligator.jpg",
-        href: ""
     },
     {
         id: 2,
@@ -19,37 +15,30 @@ const singleItems = [
         price: 126,
         condition: "Used",
         src: "/images/profileListings/shrink ray gun.jpg",
-        href: ""
     },
     {
         id: 3,
-        description: "Missle",
+        description: "Missile",
         price: 516,
         condition: "Used",
         src: "/images/profileListings/missle.jpg",
-        href: ""
     }
-]
+];
 
 export default function ProfileListings() {
     return (
         <div>
-            <Typography sx={{color: "#0033a0", padding: "10px 0px"}}>
+            <Typography sx={{ color: "#0033a0", padding: "10px 0px" }}>
                 Results (3)
             </Typography>
-            <Grid id="listings" container rowSpacing={3} columnSpacing={3}>
-                {singleItems.map(({ id, description, price, condition, src, href }) => (
-                    <Grid key={id} xs={6} sm={4} md={4}>
-                        <ListingCard
-                            description={description}
-                            price={"$" + price}
-                            condition={condition}
-                            src={src}
-                            href={href}
-                        />
+            <br/>
+            <Grid container columnSpacing={3} rowSpacing={3}>
+                {profileItems.map((item) => (
+                    <Grid key={item.id} xs={12} sm={6} md={4}>
+                        <ListingCard item={item}/>
                     </Grid>
                 ))}
             </Grid>
         </div>
-    )
+    );
 }
