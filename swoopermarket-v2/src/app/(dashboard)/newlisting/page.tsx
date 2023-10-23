9f9f2eebad3b8f54d1db10bdaf7c50b2a4498610
Mei Deng <meideng06@gmail.com>
2023-10-23 15:09:29 -0400

    refactored component into main newlisting page

--- swoopermarket-v2/src/app/(dashboard)/newlisting/page.tsx
+++ swoopermarket-v2/src/app/(dashboard)/newlisting/page.tsx
@@ -1,8 +1,10 @@
 'use client'
 
 import React, { useState} from 'react';
-import { Button, FormControl, Grid, InputAdornment, MenuItem, OutlinedInput, Paper, TextField, ThemeProvider, AppBar, Toolbar, createTheme, Typography, Box, Container} from '@mui/material';
-import PreviewListing from './preview/page';
+import { Button, FormControl, Grid, InputAdornment, MenuItem, OutlinedInput, Paper, TextField, ThemeProvider, AppBar, Toolbar, createTheme, Typography, Box, Container, Stack, Link } from '@mui/material';
+import ItemDescriptors from '@/components/SingleItem/ItemDescriptors';
+import ItemPhotos from '@/components/SingleItem/ItemPhotos';
+import CloseIcon from '@mui/icons-material/Close';
 
 export default function StarredPage() {
 
@@ -53,7 +55,7 @@ export default function StarredPage() {
   const [showPreview, setShowPreview] = useState<boolean>(false);
 
   const handlePreviewClick = () => {
-    setShowPreview(true);
+    setShowPreview(!showPreview);
   };
 
   const categories = [
@@ -266,7 +268,32 @@ export default function StarredPage() {
               }}
             >
             <Paper>
-              <PreviewListing formData={formData} setShowPreview={setShowPreview} />
+              <Box sx={{ display: 'flex', alignItems: 'flex-start', p: 3}}>
+                <div>
+                  <Stack direction="row" justifyContent="flex-start" alignItems="center" spacing={2}>
+                    <Button onClick={handlePreviewClick} sx={{borderRadius: 50, width: "fit-content"}} startIcon={<CloseIcon/>}>Close Preview</Button>
+                    <Stack direction="row">
+                      <Typography variant="body1" color="initial">Listed in category: </Typography>
+                      <Link href={'href'} variant="body1">{formData.category}</Link>
+                    </Stack>
+                  </Stack>
+                  <Grid container direction="row" spacing={2} padding={2} columns={{sm: 8, md: 12}}>
+                    <Grid item sm={8} md={7}>
+                      <ItemPhotos photos={[{id: "", src:"https://lsco.scene7.com/is/image/lsco/A34940028-alt3-pdp-lse?fmt=avif&qlt=40&resMode=bisharp&fit=crop,0&op_usm=0.6,0.6,8&wid=660&hei=726"}, {id: "", src:"https://lsco.scene7.com/is/image/lsco/A34940028-detail1-pdp?fmt=avif&qlt=40&resMode=bisharp&fit=crop,0&op_usm=0.6,0.6,8&wid=660&hei=726"}]}/>
+                    </Grid>
+                    <Grid item sm={8} md={5}>
+                      <ItemDescriptors descriptors={{
+                          listingTitle: formData.title,
+                          sellerId: "my_username",
+                          description: formData.description,
+                          price: formData.price,
+                          condition: formData.condition,
+                          pickup: formData.pickup
+                      }}/>
+                    </Grid>
+                  </Grid>
+                </div>
+              </Box>
             </Paper>
           </div>
         )}
