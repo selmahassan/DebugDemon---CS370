ac073cc3e8c2d07601a359392e1d017af79c749c
Mei Deng <meideng06@gmail.com>
2023-10-29 14:14:36 -0400

    remove DM button

--- swoopermarket-v2/src/components/SingleItem/Descriptors/ItemInterest.tsx
+++ swoopermarket-v2/src/components/SingleItem/Descriptors/ItemInterest.tsx
@@ -5,7 +5,6 @@ import AddIcon from '@mui/icons-material/Add';
 export default function ItemInterest({ seller } : { seller: string}) {
     return (
         <Stack direction="column" spacing={1.5}>
-            <Button variant="contained" sx={{borderRadius: 50, width: "fit-content"}} href="#contained-buttons">{`Message ${seller}`}</Button>
             <Button variant="outlined" sx={{borderRadius: 50, width: "fit-content"}} startIcon={<AddIcon/>}>Add to Watchlist</Button>
         </Stack>
     );
