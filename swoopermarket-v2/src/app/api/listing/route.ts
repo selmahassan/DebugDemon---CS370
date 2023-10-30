b938a41c6b1c08162939154bab0a8e7e962bd689
Kathy Ning <kathyning@Kathys-MacBook-Air-3.local>
2023-10-30 14:47:50 -0400

    modify listing GET'

--- swoopermarket-v2/src/app/api/listing/route.ts
+++ swoopermarket-v2/src/app/api/listing/route.ts
@@ -2,10 +2,11 @@ import { sql } from "@vercel/postgres";
 import { NextResponse } from "next/server";
 
 // get all listings in DB
+// get all listings in DB
 export const GET = async (req: Request, res: Response) => {
     try {
-        const messages =  await sql`SELECT * FROM product_listing;`;
-        return NextResponse.json({ messages }, { status: 200 });
+        const { rows } =  await sql`SELECT * FROM product_listing;`;
+        return NextResponse.json({ rows }, { status: 200 });
     } catch (error) {
         return NextResponse.json({ message: "Error", error }, { status: 500 });
     }
