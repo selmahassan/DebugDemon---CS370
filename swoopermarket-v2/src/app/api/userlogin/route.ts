1d587d81245fd2884ffd83bf9970662a5305c44b
selmahassan <78364187+selmahassan@users.noreply.github.com>
2023-10-30 15:13:12 -0400

    Update route.ts
    fixing error messages

--- swoopermarket-v2/src/app/api/userlogin/route.ts
+++ swoopermarket-v2/src/app/api/userlogin/route.ts
@@ -10,7 +10,7 @@ export const POST = async (req: Request, res: Response) => {
 
         // If user is not found, return an error response
         if (result.rows.length === 0) {
-            return NextResponse.json({ message: "User Not Found" }, { status: 404 });
+            return NextResponse.json({ message: "User Not Found, Please sign up" }, { status: 404 });
         }
         
         const user = result.rows[0];
@@ -22,11 +22,11 @@ export const POST = async (req: Request, res: Response) => {
         if (pass === user.pass) { 
             return NextResponse.json({ message: "Logged in successfully" }, { status: 200 });
         } else {
-            return NextResponse.json({ message: "Invalid password" }, { status: 401 });
+            return NextResponse.json({ message: "Invalid password, Please Try Again" }, { status: 401 });
         }
 
     } catch (error) {
         console.log("Caught error:", error);
-        return NextResponse.json({ message: "Error", error }, { status: 500 });
+        return NextResponse.json({ message: "Please Try Again", error }, { status: 500 });
     }
 };
