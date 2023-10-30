c7c92f6b48c68e7bbc0b0bf6fd3812d06719ee9d
Kathy Ning <kathyning@Kathys-MacBook-Air-3.local>
2023-10-29 20:28:11 -0400

    fix descr type

--- swoopermarket-v2/src/components/ProfilePage/ProfileListings.tsx
+++ swoopermarket-v2/src/components/ProfilePage/ProfileListings.tsx
@@ -1,25 +1,26 @@
 import { Typography, Grid } from "@mui/material";
 import ListingCard from '@/components/HomePage/ListingCard';
+import { ItemType } from "@/types/itemType";
 
 const profileItems = [
     {
         listing_id: 7,
         product_name: "Pet Alligator",
-        desc: "Generic Description",
+        descr: "Generic Description",
         price: 48,
         listing_img: "/images/profileListings/alligator.jpg",
     },
     {
         listing_id: 8,
         product_name: "Shrink Ray Gun",
-        description: "Generic Description",
+        descr: "Generic Description",
         price: 126,
         listing_img: "/images/profileListings/shrink ray gun.jpg",
     },
     {
         listing_id: 9,
         product_name: "Missile",
-        desc: "Generic Description",
+        descr: "Generic Description",
         price: 516,
         listing_img: "/images/profileListings/missle.jpg",
     },
@@ -31,13 +32,14 @@ export default function ProfileListings() {
             <Typography sx={{ color: "#0033a0", padding: "10px 0px" }}>
                 Results ({profileItems.length})
             </Typography>
-            <Grid container spacing={3}>
-                {profileItems.map((item) => (
-                    <Grid key={item.listing_id} item xs={6} sm={4} md={4}>
-                        <ListingCard item={item} />
+            <Grid id="personal listings" container rowSpacing={3} columnSpacing={3}>
+                {profileItems.map((item: ItemType) => (
+                    <Grid key={item.listing_id} xs={12} sm={4} md={4}>
+                        <ListingCard item={item}/>
                     </Grid>
                 ))}
             </Grid>
+            
         </div>
     );
 }
