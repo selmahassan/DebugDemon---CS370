6631db0743f5de9de00fecb2c1271cc5e642b0e3
Kathy Ning <kathyning@Kathys-MacBook-Air-3.local>
2023-11-02 18:11:59 -0400

    remove cache for GET request

--- swoopermarket-v2/src/app/(dashboard)/page.tsx
+++ swoopermarket-v2/src/app/(dashboard)/page.tsx
@@ -1,15 +1,14 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Listings from '@/components/HomePage/Listings';
-import { revalidatePath } from 'next/cache'
 
 async function getData() {
-  // TODO: make fetch non-local URL
   const res = await fetch(process.env.API_URL + 'api/listing', {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json'
     },
+    cache: 'no-store'
   });
   
   if (!res.ok) {
@@ -22,7 +21,6 @@ async function getData() {
 export default async function HomePage() {
   const res = await getData();
   let listings = res.rows
-  revalidatePath('/', 'page')
 
   return (
     <Box sx={{ display: 'flex', alignItems: 'flex-start'}}>
