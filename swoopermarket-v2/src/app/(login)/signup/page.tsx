4742ef139bd82ab69a04419df4e2e0099a5863f8
Ryan <67213962+ryan-zhao313@users.noreply.github.com>
2023-11-06 11:40:34 -0500

    Added phone number to sign up page

--- swoopermarket-v2/src/app/(login)/signup/page.tsx
+++ swoopermarket-v2/src/app/(login)/signup/page.tsx
@@ -23,12 +23,14 @@ import StickyAlert from '@/components/StickyAlert';
 
 interface NewUserForm {
     email: string;
+    phoneNumber: string;
     password: string;
     rePassword: string;
 }
 
 interface errors {
     email: string;
+    phoneNumber: string;
     rePassword: string;
 }
 
@@ -37,21 +39,32 @@ export default function SignUp() {
     const [showRePassword, setShowRePassword] = React.useState(false); // 
     const [newUserForm, setNewUserForm] = React.useState<NewUserForm>({
         email: "",
+        phoneNumber: "",
         password: "",
         rePassword: "",
     })
     const [errors, setErrors] = React.useState<errors>({
         email: "",
+        phoneNumber: "",
         rePassword: "",
     })
 
     const onChangeEmail = (e: { target: { value: string; }; }) => {
         if (!e.target.value.match(".+@emory\.edu")) {
-            setErrors({...errors, email: 'email must end in "@emory.edu"'})
+            setErrors({...errors, email: 'Email must end in "@emory.edu"'})
         } else {
             setErrors({...errors, email: ''})
         }
         setNewUserForm({...newUserForm, email: e.target.value});
+    };
+
+    const onChangePhoneNumber = (e: { target: { value: string; }; }) => {
+        if (!e.target.value.match(/^(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})$/)) {
+            setErrors({...errors, phoneNumber: 'Phone number must be valid'})
+        } else {
+            setErrors({...errors, phoneNumber: ''})
+        }
+        setNewUserForm({...newUserForm, phoneNumber: e.target.value});
     };
 
     const handleClickShowPassword = () => setShowPassword((show) => !show);
@@ -90,6 +103,7 @@ export default function SignUp() {
             firstName: data.get('firstName') as string,
             lastName: data.get('lastName') as string,
             email: data.get('email') as string,
+            phoneNumber: data.get('phoneNumber') as string,
             password: data.get('password') as string,
             bio: data.get('bio') as string,
         };
@@ -113,7 +127,18 @@ export default function SignUp() {
 
     };
 
-  return (
+    // Check if all fields are filled
+    const areAllFieldsFilled = () => {
+        return (
+            newUserForm.email &&
+            newUserForm.phoneNumber &&
+            newUserForm.password &&
+            newUserForm.rePassword
+        );
+    };
+
+  // @ts-ignore
+    return (
     <Container component="main" maxWidth="xs">
         <CssBaseline />
         <Box
@@ -176,9 +201,24 @@ export default function SignUp() {
                 />
                 </Grid>
                 <Grid item xs={12}>
+                <TextField
+                    required
+                    fullWidth
+                    id="phoneNumber"
+                    label="Phone Number"
+                    name="phoneNumber"
+                    autoComplete="tel"
+                    error={errors.phoneNumber !== ""}
+                    helperText={errors.phoneNumber}
+                    value={newUserForm.phoneNumber || ''}
+                    onChange={onChangePhoneNumber}
+                />
+                </Grid>
+                <Grid item xs={12}>
                 <FormControl required fullWidth variant="outlined">
                     <InputLabel htmlFor="password">Password</InputLabel>
                     <OutlinedInput
+                        required
                         id="password"
                         name="password"
                         type={showPassword ? 'text' : 'password'}
@@ -232,25 +272,15 @@ export default function SignUp() {
                 </FormControl>
                 </Grid>
             </Grid>
-            {(errors.rePassword || errors.email) ? 
-                <Button
-                    // type="submit"
-                    disabled
-                    fullWidth
-                    variant="contained"
-                    sx={{ mt: 3, mb: 2 }}
-                >
-                    Sign Up
-                </Button> : 
                 <Button
                     type="submit"
+                    disabled={!!errors.rePassword || !!errors.email || !areAllFieldsFilled()}
                     fullWidth
                     variant="contained"
                     sx={{ mt: 3, mb: 2 }}
                 >
                     Sign Up
                 </Button>
-            }
             <Grid container justifyContent="flex-end">
                 <Grid item>
                 <Link href="/login" style={{ textDecoration:"none" }}>
