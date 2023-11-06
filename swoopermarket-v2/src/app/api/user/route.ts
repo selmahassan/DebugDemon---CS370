8012f8ac5002cf1bd85ccf3a3421e11ccbcaf14a
selmahassan <78364187+selmahassan@users.noreply.github.com>
2023-11-06 15:05:06 -0500

    Update route.ts

--- swoopermarket-v2/src/app/api/user/route.ts
+++ swoopermarket-v2/src/app/api/user/route.ts
@@ -6,15 +6,15 @@ const SALT_ROUNDS = 10; //
 
 // add a new user to the DB with hashed password
 export const POST = async (req: Request, res: Response) => {
-    const { firstName, lastName, email, password } = await req.json();
+    const { firstName, lastName, email, password, phone } = await req.json();
 
     // Hash the password before saving to the database
     const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
 
     try {
         const result = await sql`
-            INSERT INTO user_table (first_name, last_name, email, pass)
-            VALUES (${firstName}, ${lastName}, ${email}, ${hashedPassword});
+            INSERT INTO user_table (first_name, last_name, email, pass, phone)
+            VALUES (${firstName}, ${lastName}, ${email}, ${hashedPassword}, ${phone);
         `;
         return NextResponse.json({ result }, { status: 201 });
     } catch (error) {
