d8feb3913bc95b52004ff05bce109368c155780a
selmahassan <78364187+selmahassan@users.noreply.github.com>
2023-11-06 15:07:13 -0500

    Update types.d.ts

--- swoopermarket-v2/src/types.d.ts
+++ swoopermarket-v2/src/types.d.ts
@@ -13,7 +13,7 @@ export type User = {
     firstName: string,
     lastName: string,
     email: string,
-    phoneNumber: string,
     password: string,
-    bio: string
+    bio: string,
+    phone: string,
 }
