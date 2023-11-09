b1678d2c6fb6301ebbdc2891ad28e201ad0e1bf2
Mei Deng <meideng06@gmail.com>
2023-11-08 19:01:49 -0500

    Replace "Add to Watchlist" button w/ edit, delete, share

--- swoopermarket-v2/src/components/SingleItem/Descriptors/ItemInterest.tsx
+++ swoopermarket-v2/src/components/SingleItem/Descriptors/ItemInterest.tsx
@@ -1,11 +1,15 @@
 import Stack from "@mui/material/Stack";
 import Button from '@mui/material/Button';
-import AddIcon from '@mui/icons-material/Add';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
+import ShareIcon from '@mui/icons-material/Share';
 
 export default function ItemInterest({ seller } : { seller: string}) {
     return (
-        <Stack direction="column" spacing={1.5}>
-            <Button variant="outlined" sx={{borderRadius: 50, width: "fit-content"}} startIcon={<AddIcon/>}>Add to Watchlist</Button>
+        <Stack direction="row" spacing={1.5}>
+            <Button variant="outlined" sx={{borderRadius: 50, width: "fit-content"}} startIcon={<EditIcon/>}>Edit</Button>
+            <Button variant="outlined" sx={{borderRadius: 50, width: "fit-content"}} startIcon={<DeleteIcon/>}>Delete</Button>
+            <Button variant="contained" sx={{borderRadius: 50, width: "fit-content"}} startIcon={<ShareIcon/>}>Share</Button>
         </Stack>
     );
 }
