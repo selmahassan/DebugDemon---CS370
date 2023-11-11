2a672a120e546a5af9e2d617b6c31cc14e815407
Kathy Ning <kathyning@Kathys-MacBook-Air-3.local>
2023-11-10 21:28:20 -0600

    uncomment code

--- swoopermarket-v2/src/app/(dashboard)/newlisting/page.tsx
+++ swoopermarket-v2/src/app/(dashboard)/newlisting/page.tsx
@@ -55,35 +55,35 @@ export default function StarredPage() {
     const image_url = await submitBlob(image) // TODO: Change listing_img to string to submit url
     console.log(image_url)
 
-    // // TODO : categories mistmatch with database
-    // let category_id: number = 4; // gotta initialize it bc const listing wants me to
-    // if(data.get('category') == 'school_supplies') category_id = 1; // db says apparel = 1
-    // else if (data.get('category') == 'furniture') category_id = 2;
-    // else if (data.get('category') == 'electronics') category_id = 3;
-    // else category_id = 4; // db says entertainment = 4
+    // TODO : categories mistmatch with database
+    let category_id: number = 4; // gotta initialize it bc const listing wants me to
+    if(data.get('category') == 'school_supplies') category_id = 1; // db says apparel = 1
+    else if (data.get('category') == 'furniture') category_id = 2;
+    else if (data.get('category') == 'electronics') category_id = 3;
+    else category_id = 4; // db says entertainment = 4
 
-    // const listing : Listing = {
-    //   title: data.get('title') as string,
-    //   description: data.get('description') as string,
-    //   category: category_id,
-    //   condition: data.get('condition') as string,
-    //   price: Number(data.get('price')), // TODO : frontend: can you somehow make sure what the user enters as price is a number only?
-    //   pickup: data.get('pickup') as string
-    // };
+    const listing : Listing = {
+      title: data.get('title') as string,
+      description: data.get('description') as string,
+      category: category_id,
+      condition: data.get('condition') as string,
+      price: Number(data.get('price')), // TODO : frontend: can you somehow make sure what the user enters as price is a number only?
+      pickup: data.get('pickup') as string
+    };
 
-    // let response = await fetch('../api/listing', {
-    //   method: 'POST',
-    //   headers: {
-    //     'Content-Type': 'application/json'
-    //   },
-    //   body: JSON.stringify(listing)
-    // });
+    let response = await fetch('../api/listing', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(listing)
+    });
 
-    // if(response.status == 200) {
-    //   setOpenError(true);
-    // } else {
-    //   setOpenSuccess(true);
-    // }
+    if(response.status == 200) {
+      setOpenError(true);
+    } else {
+      setOpenSuccess(true);
+    }
   };
 
   const theme = createTheme({
