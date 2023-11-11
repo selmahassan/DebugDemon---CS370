e6fa7645802c5349a5ef03df771feedf7d5a4fe4
Kathy Ning <kathyning@Kathys-MacBook-Air-3.local>
2023-11-11 07:23:16 -0600

    modify images POST route

--- swoopermarket-v2/src/app/api/images/route.ts
+++ swoopermarket-v2/src/app/api/images/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from 'next/server';
 export async function POST(request: Request): Promise<NextResponse> {
   const { searchParams } = new URL(request.url);
   const filename = searchParams.get('filename') as string;
-  const request_body = JSON.stringify(request.body)
+  const request_body = request.body as ReadableStream<Uint8Array>;
 
   const blob = await put(filename, request_body, {
     access: 'public',
