57c935a08b69c99f2384e2b9e03b6efc4dc27608
Kathy Ning <kathyning@Kathys-MacBook-Air-3.local>
2023-11-10 21:41:01 -0600

    Revert "fix post"
    This reverts commit 176e472d540335f30bb239d7ffb56d8e6a539066.

--- swoopermarket-v2/src/app/api/listing/route.ts
+++ swoopermarket-v2/src/app/api/listing/route.ts
@@ -14,15 +14,14 @@ export const GET = async (req: Request, res: Response) => {
 
 // post a new listing into DB
 export const POST = async (req: Request, res: Response) => {
-    const { searchParams } = new URL(req.url);
+    const {listingid, title, description, image, category, condition, price, pickup} = await req.json();
 
-    const {title, description, image, category, condition, price, pickup, img} = await req.json();
-
-    let user_id = 1 // TODO : how to fetch user id from newlisting page? - Will use cookies
+    let listing_id = 19
+    let user_id = "selma" // TODO : how to fetch user id from newlisting page?
 
     try {// TODO: product_listing table doesnt have a condition or pickup column
-        const messages = await sql`INSERT INTO product_listing (Userid, Product_name, Descr, Category_id, Price, Created_at, Modified_at) 
-        VALUES (${user_id}, ${title}, ${description}, ${category}, ${price}, NOW(), NOW());`;
+        const messages = await sql`INSERT INTO product_listing (Listing_id, Userid, Product_name, Descr, Category_id, Price, Created_at, Modified_at) 
+        VALUES (${listing_id}, ${user_id}, ${title}, ${description}, ${category}, ${price}, NOW(), NOW());`;
                 
         return NextResponse.json({ messages }, { status: 200 });
     } catch (error) {
