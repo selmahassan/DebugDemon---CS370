24e21587a23d83ef2450de7023e0f4cf7674d2f9
Kathy Ning <kathyning@Kathys-MacBook-Air-3.local>
2023-11-10 21:35:00 -0500

    specify filename as string

--- swoopermarket-v2/src/app/api/images/route.ts
+++ swoopermarket-v2/src/app/api/images/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 
 export async function POST(request: Request): Promise<NextResponse> {
   const { searchParams } = new URL(request.url);
-  const filename = searchParams.get('filename');
+  const filename = searchParams.get('filename') as string
 
   const blob = await put(filename, request.body, {
     access: 'public',
