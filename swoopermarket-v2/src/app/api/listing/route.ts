e592b42d9b615e9262b513f3e09c63c08652b158
Kathy Ning <kathyning@Kathys-MacBook-Air-3.local>
2023-11-10 21:36:58 -0600

    remove listing id

--- swoopermarket-v2/src/app/api/listing/route.ts
+++ swoopermarket-v2/src/app/api/listing/route.ts
@@ -15,13 +15,11 @@ export const GET = async (req: Request, res: Response) => {
 // post a new listing into DB
 export const POST = async (req: Request, res: Response) => {
     const {title, description, image, category, condition, price, pickup} = await req.json();
-
-    let listing_id = 19
-    let user_id = "selma" // TODO : how to fetch user id from newlisting page?
+    let user_id = 1
 
     try {// TODO: product_listing table doesnt have a condition or pickup column
-        const messages = await sql`INSERT INTO product_listing (Listing_id, Userid, Product_name, Descr, Category_id, Price, Created_at, Modified_at) 
-        VALUES (${listing_id}, ${user_id}, ${title}, ${description}, ${category}, ${price}, NOW(), NOW());`;
+        const messages = await sql`INSERT INTO product_listing (Userid, Product_name, Descr, Category_id, Price, Created_at, Modified_at) 
+        VALUES (${user_id}, ${title}, ${description}, ${category}, ${price}, NOW(), NOW());`;
                 
         return NextResponse.json({ messages }, { status: 200 });
     } catch (error) {
