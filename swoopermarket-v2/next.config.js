b138cee5219be34ae9d078f5abdef259b1fb754c
Kathy Ning <kathyning@Kathys-MacBook-Air-3.local>
2023-11-12 13:31:36 -0500

    allow all images in next.config

--- swoopermarket-v2/next.config.js
+++ swoopermarket-v2/next.config.js
@@ -1,22 +1,33 @@
 /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+//   modularizeImports: {
+//     '@mui/icons-material': {
+//       transform: '@mui/icons-material/{{member}}',
+//     },
+//   },
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'source.unsplash.com',
+//         port: '',
+//         pathname: '/random',
+//       },
+//     ],
+//     domains: ["static.nike.com", "lsco.scene7.com", "images.unsplash.com", "m.media-amazon.com"]
+//   },
+// };
+
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  modularizeImports: {
-    '@mui/icons-material': {
-      transform: '@mui/icons-material/{{member}}',
-    },
-  },
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'source.unsplash.com',
-        port: '',
-        pathname: '/random',
+        protocol: "https",
+        hostname: "**",
       },
     ],
-    domains: ["static.nike.com", "lsco.scene7.com", "images.unsplash.com", "m.media-amazon.com"]
   },
 };
 
