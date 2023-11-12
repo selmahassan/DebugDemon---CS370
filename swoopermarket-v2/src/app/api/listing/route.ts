1bdeb946dd2a0aa3b27029a8230c459a67ac0932
Kathy Ning <kathyning@Kathys-MacBook-Air-3.local>
2023-11-12 10:48:45 -0500

    add vars to newlisting post

--- swoopermarket-v2/src/app/api/listing/route.ts
+++ swoopermarket-v2/src/app/api/listing/route.ts
@@ -15,11 +15,11 @@ export const GET = async (req: Request, res: Response) => {
 // post a new listing into DB
 export const POST = async (req: Request, res: Response) => {
     const {title, description, image, category, condition, price, pickup} = await req.json();
-    let user_id = 1
+    let user_id = 1 // TODO: remove hardcoded user-id
 
     try {// TODO: product_listing table doesnt have a condition or pickup column
-        const messages = await sql`INSERT INTO product_listing (Userid, Product_name, Descr, Category_id, Price, Created_at, Modified_at) 
-        VALUES (${user_id}, ${title}, ${description}, ${category}, ${price}, NOW(), NOW());`;
+        const messages = await sql`INSERT INTO product_listing (Userid, Product_name, Descr, Category_id, Price, Created_at, Modified_at, Pickup, Condition) 
+        VALUES (${user_id}, ${title}, ${description}, ${category}, ${price}, NOW(), NOW(), ${pickup}, ${condition});`;
                 
         return NextResponse.json({ messages }, { status: 200 });
     } catch (error) {
