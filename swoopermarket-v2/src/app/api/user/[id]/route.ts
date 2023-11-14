8a06f78cb9f38bd183c0f2edd104641202ddf46e
Kathy Ning <kathyning@Kathys-MacBook-Air-3.local>
2023-11-14 09:55:34 -0500

    add GET_LISTINGS api for user

--- swoopermarket-v2/src/app/api/user/[id]/route.ts
+++ swoopermarket-v2/src/app/api/user/[id]/route.ts
@@ -21,6 +21,26 @@ export const GET = async (req: Request) => {
 
 };
 
+// get all user listings in DB
+export const GET_LISTINGS = async (req: Request) => {
+    try {
+        const url_id = req.url.split("api/user/")[1];
+        
+        const messages =  await sql`SELECT * FROM product_listing WHERE userid = ${url_id};`; 
+        const listings = messages.rows;
+
+        if(listings.length == 0){
+            return NextResponse.json({message: "No listings ", listings}, {status: 200});
+        } else {
+            return NextResponse.json({message: "Messages: ", listings}, {status: 200});
+        }
+
+    } catch (error) {
+        return NextResponse.json({message: "Error", error}, {status: 500});
+    }
+
+};
+
 // edit a user's info
 export const PUT = async (req: Request, res: Response) => {
     const {user_id, pass, first_name, last_name} = await req.json();
