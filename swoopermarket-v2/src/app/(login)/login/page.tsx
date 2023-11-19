56587d115ff48a27018eca94526a6cf4804338c4
Mei Deng <meideng06@gmail.com>
2023-11-19 16:12:35 -0500

    login page edits

--- swoopermarket-v2/src/app/(login)/login/page.tsx
+++ swoopermarket-v2/src/app/(login)/login/page.tsx
@@ -4,8 +4,6 @@ import React, { useState, useEffect } from 'react';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
 import Link from 'next/link';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
@@ -23,25 +21,48 @@ import { Visibility, VisibilityOff } from '@mui/icons-material';
 import StickyAlert from '@/components/StickyAlert';
 
 export default function Login() {
-  const [showPassword, setShowPassword] = React.useState(false);
   const router = useRouter()
+  const searchParams = useSearchParams()
+  const isSuccess = searchParams.get('isSuccess')
+
+  const [showPassword, setShowPassword] = React.useState(false);
+  const [openSuccess, setOpenSuccess] = useState(false);
+  const [openError, setOpenError] = useState(false);
+  const [errorMessage, setErrorMessage] = React.useState('');
 
   const handleClickShowPassword = () => setShowPassword((show) => !show);
   const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
   };
 
-  const searchParams = useSearchParams()
-  const search = searchParams.get('isSuccess')
-  const [openSuccess, setOpenSuccess] = useState(false);
-  const [openError, setOpenError] = useState(false);
-  const [errorMessage, setErrorMessage] = React.useState('');
-
   useEffect(() => {
-    if (search === 'true') {
+    if (isSuccess === 'true') {
       setOpenSuccess(true);
     }
-  }, [search]);
+  }, [isSuccess]);
+
+  const verificationToken = searchParams.get('token')
+  useEffect(() => {
+    const verifyToken = async () => {
+      if (verificationToken) {
+        try {
+          const response = await fetch(`/api/verify?token=${verificationToken}`);
+          if (response.ok) {
+            const data = await response.json();
+            console.log(data.message); // Do something with the response message
+          } else {
+            console.error('Failed to verify token');
+            // Handle unsuccessful verification here
+          }
+        } catch (error) {
+          console.error('Error during token verification:', error);
+          // Handle error cases here
+        }
+      }
+    };
+
+    verifyToken();
+  }, [[verificationToken]])
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
   event.preventDefault();
