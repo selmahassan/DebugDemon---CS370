31ab19dcb85d81ce4f1bfa32b82cbbf72abf24b3
selmahassan <78364187+selmahassan@users.noreply.github.com>
2023-11-19 19:10:31 -0500

    Update route.ts

--- swoopermarket-v2/src/app/api/user/route.ts
+++ swoopermarket-v2/src/app/api/user/route.ts
@@ -32,18 +32,18 @@ export const POST = async (req: Request, res: Response) => {
             },
         });
 
-        // await new Promise((resolve, reject) => {
-        //     // verify connection configuration
-        //     transporter.verify(function (error, success) {
-        //         if (error) {
-        //             console.log(error);
-        //             reject(error);
-        //         } else {
-        //             console.log("Server is ready to take our messages");
-        //             resolve(success);
-        //         }
-        //     });
-        // });
+        await new Promise((resolve, reject) => {
+            // verify connection configuration
+            transporter.verify(function (error, success) {
+                if (error) {
+                    console.log(error);
+                    reject(error);
+                } else {
+                    console.log("Server is ready to take our messages");
+                    resolve(success);
+                }
+            });
+        });
 
         await transporter.verify();
         // Email user a verification link
