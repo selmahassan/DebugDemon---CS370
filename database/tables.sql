5a7673faf9d346d7b0bcb9fe021c847df4626143
Kathy Ning <kathyning@Kathys-MacBook-Air-3.local>
2023-11-26 16:55:00 -0600

    revise sql queries

--- database/tables.sql
+++ database/tables.sql
@@ -1,35 +1,25 @@
 CREATE TABLE user_table (
   userid SERIAL PRIMARY KEY,
-  pass text,
-  first_name varchar(255),
-  last_name varchar(255),
-  email text UNIQUE, -- Assuming each email should be unique
-  phone text,
-  profile_img BYTEA
+  pass TEXT,
+  first_name VARCHAR(255),
+  last_name VARCHAR(255),
+  email TEXT UNIQUE, -- Assuming each email should be unique
+  phone TEXT,
+  bio TEXT,
+  verification_token VARCHAR(128) UNIQUE,
+  verified BOOLEAN
 );
 
 CREATE TABLE password_reset_tokens (
-  email text,
+  email TEXT,
   token VARCHAR(128) UNIQUE,
   token_expiry BIGINT,    
   PRIMARY KEY (email, token) -- An email can have more than one expiry token
 );
 
-INSERT INTO user_table (pass, first_name, last_name, email, phone) VALUES ('selma', 'selma', 'hassan', 'selma@emory.edu', '4044512184');
-INSERT INTO user_table (pass, first_name, last_name, email, phone) VALUES ('ryan', 'ryan', 'zhao', 'ryan@emory.edu', '4044512184');
-CREATE TABLE user_payment (
-    id int PRIMARY KEY,
-    userid SERIAL,
-    FOREIGN KEY (userid) REFERENCES user_table(userid),
-    payment_type varchar(255),
-    payment_provider varchar(255),
-    account_no int,
-    expiry date
-)
-
 CREATE TABLE product_category(
-  category_id varchar PRIMARY KEY,
-  category_name varchar
+  category_id VARCHAR PRIMARY KEY,
+  category_name VARCHAR
 )
 
 INSERT INTO product_category (category_id, category_name) VALUES (1, "School Supplies"), (2, "Furniture"), (3, "Electronics"), (4, "Other"), (5, "Tickets"), (6, "Housing"), (7, "Books")
@@ -38,55 +28,35 @@ CREATE TABLE product_listing (
     listing_id SERIAL PRIMARY KEY,
     userid SERIAL,
     FOREIGN KEY (userid) REFERENCES user_table(userid),
-    product_name varchar,
-    descr text,
-    category_id varchar,
+    product_name VARCHAR,
+    descr TEXT,
+    category_id VARCHAR,
     FOREIGN KEY (category_id) REFERENCES product_category(category_id),
-    inventory_id int,
-    price float,
-    created_at timestamp,
-    modified_at timestamp,
-    sold_at timestamp,
-    listing_img BYTEA
-    pickup varchar,
-    condition varchar
-)
-INSERT INTO product_listing (userid, product_name, descr, category_id, inventory_id, price) VALUES (1, 'Laptop', 'New macbook 2023', 3, 1, 1050);
-
-
-CREATE TABLE transaction_details (
-  transaction_id int PRIMARY KEY,
-  sale_amount float,
-  payment_id int,
-  FOREIGN KEY (payment_id) REFERENCES user_payment(ID)
+    inventory_id INT,
+    price FLOAT,
+    created_at TIMESTAMP,
+    modified_at TIMESTAMP,
+    sold_at TIMESTAMP,
+    listing_img VARCHAR,
+    pickup VARCHAR,
+    condition VARCHAR,
+    sold BOOLEAN
 )
 
-CREATE TABLE order_details(
-  order_id int,
-  userid SERIAL,
-  FOREIGN KEY (userid) REFERENCES user_table(userid),
-  listing_id int,
+CREATE TABLE comments_table (
+  comment_id SERIAL PRIMARY KEY,
+  comment_text TEXT,
+  created_at TIMESTAMP,
+  listing_id SERIAL,
   FOREIGN KEY (listing_id) REFERENCES product_listing(listing_id),
-  transaction_id int,
-  FOREIGN KEY (transaction_id) REFERENCES transaction_details(transaction_id),
-  order_status varchar,
-  created_at timestamp
+  user_id SERIAL,
+  FOREIGN KEY (user_id) REFERENCES user_table(userid),
+  user_name VARCHAR(255) -- References a user's first name
 )
 
-CREATE TABLE messages (
-  message_id int,
-  userid SERIAL,
-  FOREIGN KEY (userid) REFERENCES user_table(userid),
-  listing_id int,
-  FOREIGN KEY (listing_id) REFERENCES product_listing(listing_id),
-  text_msg text
-)
-
-
-DROP TABLE messages;
-DROP TABLE order_details;
-DROP TABLE transaction_details;
+-- Drop all tables
+DROP TABLE comments_table;
 DROP TABLE product_listing;
 DROP TABLE product_category;
-DROP TABLE user_payment;
+DROP TABLE password_reset_tokens;
 DROP TABLE user_table;
